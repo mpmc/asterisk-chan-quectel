0f76ec81ffcdb34cc336b8aa830502ff9f3991c2
Edmunt Pienkowsky <roed@onet.eu>
2023-04-30 17:50:47 +0200

    Extend 'show device settings' command
    Show missed configuration switches: multiparty, dtmf.

--- cli.c
+++ cli.c
@@ -143,6 +143,8 @@ static char* cli_show_device_settings (struct ast_cli_entry* e, int cmd, struct 
 		ast_cli (a->fd, "  Disable SMS             : %s\n", AST_CLI_YESNO(CONF_SHARED(pvt, disablesms)));
 		ast_cli (a->fd, "  Reset Quectel           : %s\n", AST_CLI_YESNO(CONF_SHARED(pvt, resetquectel)));
 		ast_cli (a->fd, "  Call Waiting            : %s\n", dc_cw_setting2str(CONF_SHARED(pvt, callwaiting)));
+		ast_cli (a->fd, "  Multiparty Calls        : %s\n", AST_CLI_YESNO(CONF_SHARED(pvt, multiparty)));
+		ast_cli (a->fd, "  DTMF Detection          : %s\n", AST_CLI_YESNO(CONF_SHARED(pvt, dtmf)));
 		ast_cli (a->fd, "  Initial device state    : %s\n\n", dev_state2str(CONF_SHARED(pvt, initstate)));
 
 		ast_mutex_unlock (&pvt->lock);
