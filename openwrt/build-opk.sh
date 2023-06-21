1d8aad8234625b70965f78d45617e1c72efe88f4
Edmunt Pienkowsky <roed@onet.eu>
2023-06-21 10:08:56 +0200

    Improve OpenWRT build script

--- openwrt/build-opk.sh
+++ openwrt/build-opk.sh
@@ -3,17 +3,21 @@
 PKG_NAME=asterisk-chan-quectel
 
 if [ -f .config ]; then
+    # rebuild
     make package/${PKG_NAME}/clean
-    make package/${PKG_NAME}/compile
-    make package/index
 else
-    cat feeds-strskx.conf >> feeds.conf.default
+    # configure SDK first
+    cat feeds.conf.default feeds-strskx.conf >> feeds.conf
     ./scripts/feeds update -a
     ./scripts/feeds install -a
     ./scripts/feeds install ${PKG_NAME}
 
     cp diffconfig .config
     make defconfig
-    make package/${PKG_NAME}/compile
-    make package/index
 fi
+
+make package/${PKG_NAME}/compile
+make package/index
+
+IPK=$(ls bin/packages/*/strskx/*.ipk)
+echo "Package: $IPK"
