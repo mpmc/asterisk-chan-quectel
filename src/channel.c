fd6ae80ee07758300b70a71d78b7e623d1d23f44
Edmunt Pienkowsky <roed@onet.eu>
2023-10-22 10:42:24 +0200

    alsa: capture - do not read after prepare

--- src/channel.c
+++ src/channel.c
@@ -659,15 +659,12 @@ static struct ast_frame* channel_read_uac(struct cpvt* cpvt, struct pvt* pvt, si
     const snd_pcm_state_t state = snd_pcm_state(pvt->icard);
     switch (state) {
         case SND_PCM_STATE_XRUN:
-            ast_log(LOG_WARNING, "[%s][ALSA][CAPTURE] Device state: %s", PVT_ID(pvt), snd_pcm_state_name(state));
-
         case SND_PCM_STATE_SETUP: {
             const int res = snd_pcm_prepare(pvt->icard);
             if (res) {
                 ast_log(LOG_ERROR, "[%s][ALSA][CAPTURE] Prepare failed - state:%s err:'%s'\n", PVT_ID(pvt), snd_pcm_state_name(state), snd_strerror(res));
-                return NULL;
             }
-            break;
+            return NULL;
         }
 
         case SND_PCM_STATE_PREPARED:
@@ -878,8 +875,6 @@ static int channel_write_uac(struct ast_channel*, struct ast_frame* f, struct cp
     const snd_pcm_state_t state = snd_pcm_state(pvt->ocard);
     switch (state) {
         case SND_PCM_STATE_XRUN:
-            ast_log(LOG_WARNING, "[%s][ALSA][PLAYBACK] Device state: %s\n", PVT_ID(pvt), snd_pcm_state_name(state));
-
         case SND_PCM_STATE_SETUP:
             res = snd_pcm_prepare(pvt->ocard);
             if (res) {
