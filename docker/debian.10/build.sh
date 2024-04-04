b169debcca9960e3123f010f6edb7726e7ceb273
Edmunt Pienkowsky <roed@onet.eu>
2024-04-04 12:10:05 +0200

    docker: Improve Dockerfiles

--- docker/debian.10/build.sh
+++ docker/debian.10/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
 
-readonly IMGTAG=asterisk-chan-quectel/debian-bullseye:v1
+BUILD_TARGET=$(basename $0)
+BUILD_TARGET=${BUILD_TARGET%.sh}
+BUILD_TARGET=${BUILD_TARGET#build}
+
+readonly IMGTAG=asterisk-chan-quectel/debian-buster:v1
 
 docker image build \
     --build-arg BRANCH=$(git branch --show-current) \
     --tag=$IMGTAG \
-    --target=chan-quectel \
+    --target=chan-quectel${BUILD_TARGET} \
     .
 
 CNTID=$(docker container create $IMGTAG)
